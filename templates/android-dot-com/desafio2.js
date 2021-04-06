0a5f44ba4ed455213624e995b61ef6b48f5f06f4
VictorBenachio <victor.borges04b@gmail.com>
2021-04-06 00:51:05 -0300

    implementação do dataLayer

--- templates/android-dot-com/desafio2.js
+++ templates/android-dot-com/desafio2.js
@@ -1,17 +1,288 @@
-//Desafio 2.0
-
-//CAPTURANDO O ELEMENTO HTML USANDO UM ATRIBUTO AO INVÉS DA CLASSE PARA PRATICAR O USO DO DATA ATTRIBUTE:
-var viewSource = document.querySelector("[teste]");
-
-//ADICIONANDO UM ESCUTADOR NO ELEMENTO DO BOTÃO VIEW SOURCE, QUE AO SER CLICADO IRÁ COLOCAR A VARIÁVEL E O EVENTO NO DATALAYER:
-logo.addEventListener('click', function(){
-
-     window.dataLayer = window.dataLayer || [];
+//logo
+window.dataLayer = window.dataLayer || [];
        window.dataLayer.push({
-          'event': 'seuEvento',
-          'eventCategory': 'suaCategoria',
-          'eventAction': 'suaAção',
-          'eventLabel': 'seuLabel'
+          'event': 'eventoTeste',
+          'eventCategory': 'site-android:geral',
+          'eventAction': 'clique:header',
+          'eventLabel': 'logo'
   });
 
-})
+//phone
+window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      'event' : 'eventoTeste',
+      'eventCategory' : 'site-android:geral',
+      'eventAction' : 'clique:header:menu',
+      'eventLabel' : 'phone'
+
+       )}
+
+//tablets
+window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+    'event' : 'eventoTeste',
+    'eventCategory' : 'site-android:geral',
+    'eventAction': 'clique:header:menu',
+    'eventLabel' : 'tablets'
+    
+    )}
+
+//wear
+ window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+    'event' : 'eventoTeste',
+    'eventCategory' : 'site-android:geral',
+    'eventAction' : 'clique:header:menu',
+    'eventLabel' : 'wear' 
+    
+    )}
+
+//tv
+window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+     'event' : 'eventoTeste',
+     'eventCategory' : 'site-android:geral',
+     'eventAction' : 'clique:header:menu',
+     'eventLabel' : 'tv' 
+     
+     )}
+     
+//auto
+ window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        'event' : 'eventoTeste',
+        'eventCategory' : 'site-android:geral',
+        'eventAction' : 'clique:header:menu',
+        'eventLabel' : 'auto'
+        
+        )}
+
+//one
+window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({ 
+        'event' : 'eventoTeste',
+        'eventCategory' : 'site-android:geral',
+        'eventAction' : 'clique:header:menu',
+        'eventLabel' : 'one'
+        
+        )}
+        
+//play
+window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        'event' : 'eventoTeste',
+        'eventCategory' : 'site-android:geral',
+        'eventAction' : 'clique:header:menu',
+        'eventLabel' : 'play'
+        
+        )}       
+        
+//botãoDePesquisa
+window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        'event' : 'eventoTeste',
+        'eventCategory' : 'site-android:geral',
+        'eventAction' : 'clique:header',
+        'eventLabel' : 'botão-de-pesquisa' 
+        
+        )}
+        
+//menuTrêspontinhos       
+window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        'event' : 'eventoTeste',
+        'eventCategory' : 'site-android:geral',
+        'eventAction' : 'clique:header',
+        'eventLabel' : 'menu-tres-pontinhos' 
+        
+        )}
+        
+//createYourAndroidCharacter
+window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        'event' : 'eventoTeste',
+        'eventCategory' : 'site-android:be-together-not-the-same',
+        'eventAction' : 'clique:Link',
+        'eventLabel' :   'create-your-android-character'
+        
+        )}
+        
+//botãoSetinhaBaixo
+window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        'event' : 'eventoTeste',
+        'eventCategory' : 'site-android:be-together-not-the-same',
+        'eventAction' : 'clique:botão',
+        'eventLabel' : 'botão-azul-seta-pra-baixo' 
+        
+        )}
+        
+//androidWear
+window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        'event' : 'eventoTeste',
+        'eventCategory' :'site-android:powering-screens-of-all-sizes',
+        'eventAction' : 'clique:botão:imagem',
+        'eventLabel' : 'android-wear' 
+        
+        )}
+        
+//phones
+window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        'event' : 'eventoTeste',
+        'eventCategory' : 'site-android:powering-screens-of-all-sizes',
+        'eventAction' :'clique:botão:imagem',
+        'eventLabel' : 'phones'
+        
+        )}
+
+//tablets
+window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        'event' : 'eventoTeste',
+        'eventCategory' : 'site-android:powering-screens-of-all-sizes',
+        'eventAction' : 'clique:botão:imagem',
+        'eventLabel' : 'tablets'
+        
+        )}        
+
+//androidTv
+window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        'event' : 'eventoTeste',
+        'eventCategory' : 'site-android:powering-screens-of-all-sizes',
+        'eventAction' : 'clique:botão:imagem',
+        'eventLabel' : 'android-tv'
+        
+        )}
+        
+//comingSoonAndroidAuto
+window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        'event' : 'eventoTeste',
+        'eventCategory' : 'site-android:powering-screens-of-all-sizes',
+        'eventAction' : 'clique:botão:imagem',
+        'eventLabel' : 'andoid-tv' 
+        
+        )}
+
+//seeWhatsNewInThePlayStore
+window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        'event' : 'eventoTeste',
+        'eventCategory' : 'site-android:the-best-of-google-built-in',
+        'eventAction' : 'clique:link',
+        'eventLabel' : 'see-whats-new-in-the-play-store' 
+        
+        )}
+        
+//customiseYourPhone
+window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        'event' : 'eventoTeste',
+        'eventCategory' : 'site-android:customised-by-you-for-you',
+        'eventAction' : 'clique:link',
+        'eventLabel' : 'customise-your-phone'   
+        
+        )}
+    
+//makeTheSwitch
+window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        'event' : 'eventoTeste',
+        'eventCategory' : 'site-android:more-from-android',
+        'eventAction' : 'clique:card',
+        'eventLabel' : 'make-the-switch' 
+        
+        )}
+        
+//androidIfCom
+window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        'event' : 'eventoTeste',
+        'eventCategory' : 'site-andoird:more-from-android',
+        'eventAction' : 'clique:card',
+        'eventLabel' : 'android-if-com' 
+        
+        )}
+
+//downloadNow
+window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        'event' : 'eventoTeste',
+        'eventCategory' : 'site-android:more-from-android',
+        'eventAction' : 'clique:card',
+        'eventLabel' : 'download-now' 
+        
+        )}
+        
+//findApps
+window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        'event' : 'eventoTeste',
+        'eventCategory' : 'site-android:more-from-android',
+        'eventAction' : 'clique:card',
+        'eventLabel' : 'find-apps' 
+        
+        )}
+        
+//versions
+window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        'event' : 'eventoTeste',
+        'eventCategory' : 'site-android:geral',
+        'eventAction' : 'clique:footer:link',
+        'eventLabel' : 'versions' 
+        
+        )}
+        
+//forDevelopers
+window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        'event' : 'eventoTeste',
+        'eventCategory' : 'site-android:geral',
+        'eventAction' : 'clique:footer:link',
+        'eventLabel' : 'for-developers' 
+        
+        )}
+        
+//blog
+window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        'event' : 'eventoTeste',
+        'eventCategory' : 'site-android:geral',
+        'eventAction' : 'clique:footer:link',
+        'eventLabel' : 'blog' 
+        
+        )}
+        
+//privacyPolicy
+window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        'event' : 'eventoTeste',
+        'eventCategory' : 'site-android:geral',
+        'eventAction' : 'clique:footer:link',
+        'eventLabel' : 'privacy-policy' 
+        
+        )}
+        
+//backToTop
+window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        'event' : 'eventoTeste',
+        'eventCategory' : 'site-android:geral',
+        'eventAction' : 'clique:footer:botao',
+        'eventLabel' : 'back-to-top' 
+        
+        )}
+        
+//viewSourcer
+window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        'event' : 'eventoTeste',
+        'eventCategory' : 'site-android:geral',
+        'eventAction' : 'clique:botao',
+        'eventLabel' : 'view-source' 
+        
+        )}
